1d71f806200f434a042a35fbdeefa24fe668019f
Renan Ciarlo <renanciarlo636@gmail.com>
2023-10-04 12:28:34 -0300

    exemplos de condições adicionados

--- javabootcamp/src/Operadores.java
+++ javabootcamp/src/Operadores.java
@@ -1,8 +1,28 @@
 public class Operadores{
     public static void main(String[] args) {
         int numero= 5;
+        // repeticao
+        numero++;
+        //numero = numero +1;
 
-        sysout 
+        System.out.println(numero);
+
+
+        /*Exemplo de if else mais pratico:
+         int a,b;
+         a = 5;
+         b = 6;
+
+         String resultado = a==b ?"verdadeiro" : "falso"; (o String sempre vai esperar um resultado"boolean")
+         Caso for um "Int" = int resultado = a == b ? 1 : 0;   o "==" significa "se for igual é 1 e se for falso 0"
+                                            
+
+         System.out.println(resultado)
+         'falso'
+
+         */
+
+        
 
     }
 }
