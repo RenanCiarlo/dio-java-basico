ba206a4cb3f0661b043bd8fcb9a35abd52e742ab
Renan Ciarlo <renanciarlo636@gmail.com>
2023-10-04 12:40:39 -0300

    exemplos de variaveis

--- javabootcamp/src/TiposVariaveis.java
+++ javabootcamp/src/TiposVariaveis.java
@@ -1,12 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-       int idade; // Declaração de uma variável inteira chamada "idade"
+     /* int idade; Declaração de uma variável inteira chamada "idade"
 
-       idade = 25; // Atribuição de um valor à variável "idade"
+       idade = 25; Atribuição de um valor à variável "idade"
 
-       int numero = 10; // Declaração e inicialização de uma variável
+       int numero = 10; Declaração e inicialização de uma variável
 
-       final double PI = 3.14159; // Exemplo de uma constante
+       final double PI = 3.14159; Exemplo de uma constante */  
 
 
 
