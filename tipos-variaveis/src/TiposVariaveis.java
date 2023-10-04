9cc68ce82f3cabe9b481b9b34bece24a0fc00b27
Renan Ciarlo <renanciarlo636@gmail.com>
2023-10-04 10:16:24 -0300

    Adicionando alguns tipos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       int idade; // Declaração de uma variável inteira chamada "idade"
+
+       idade = 25; // Atribuição de um valor à variável "idade"
+
+       int numero = 10; // Declaração e inicialização de uma variável
+
+       final double PI = 3.14159; // Exemplo de uma constante
+
+
+
+
     }
 }
